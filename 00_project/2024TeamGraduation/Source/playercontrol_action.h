636218e61a00990580bb9453f7f69676d2037191
KaiTakada <221104@st.yoshida-g.ac.jp>
2024-10-15 09:19:43 +0900

    プレイヤーの行動管理クラス追加。

---
+++ 00_project/2024TeamGraduation/Source/playercontrol_action.h
@@ -0,0 +1,51 @@
+//=============================================================================
+// 
+//  プレイヤーコントロールヘッダー [playercontrol.h]
+//  Author : 相馬靜雅
+// 
+//=============================================================================
+
+#ifndef _PLAYERCONTROL_H_
+#define _PLAYERCONTROL_H_	// 二重インクルード防止
+
+#define BRESSRANGE (0)
+
+#include "player.h"
+
+class CSuffocation;
+class CEffekseerObj;
+
+//==========================================================================
+// プレイヤーコントロールクラス定義
+//==========================================================================
+//=============================
+// 移動
+//=============================
+class CPlayerControlMove
+{
+public:
+
+	// コンストラクタ
+	CPlayerControlMove();
+
+	virtual void Move(CPlayer* player, const float fDeltaTime, const float fDeltaRate, const float fSlowRate);	// 移動
+
+private:
+	
+	//=============================
+	// メンバ関数
+	//=============================
+	void Blink(CPlayer* player, const float fDeltaTime, const float fDeltaRate, const float fSlowRate);	// ブリンク
+	CPlayer::SDashInfo Trigger(CPlayer* player, CPlayer::DashAngle angle);	// ダッシュ時のトリガー
+
+	//=============================
+	// メンバ変数
+	//=============================
+	int m_nCntTrigger[CPlayer::DashAngle::ANGLE_MAX];	// トリガーのカウント
+	CPlayer::DashAngle m_HoldDashAngle;					// 保持してるダッシュの移動方向
+	float m_fInputInterval;								// 入力の受け付け猶予
+	float m_fTriggerInterval;							// トリガーのインターバル
+};
+
+
+#endif
