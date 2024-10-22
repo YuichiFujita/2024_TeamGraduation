f8c5404f9bb1ff9ae2a884ae888ad7607910b74b
Seiga Soum <221120@st.yoshida-g.ac.jp>
2024-10-22 09:49:21 +0900

    やった係数やばいのあったので修正

--- 00_project/2024TeamGraduation/Source/playercontrol_move.h
+++ 00_project/2024TeamGraduation/Source/playercontrol_move.h
@@ -27,7 +27,16 @@ public:
 
 	virtual void Move(CPlayer* player, const float fDeltaTime, const float fDeltaRate, const float fSlowRate);	// 移動
 
-	float GetTriggerInterval() { return m_fTriggerInterval; }									//トリガーのインターバル取得
+	void SetCntTrigger(int nIdx, int nCnt) { m_nCntTrigger[nIdx] = nCnt; }						// トリガーのカウント設定
+	void SetCntTrigger(int* nTrigger);															// トリガーのカウント設定
+	int GetCntTrigger(int nIdx) { return m_nCntTrigger[nIdx]; }									// トリガーのカウント取得
+	int* GetCntTrigger() { return &m_nCntTrigger[0]; }											// トリガーのカウント取得
+	void SetHoldDashAngle(CPlayer::DashAngle angle) { m_HoldDashAngle = angle; }				// ダッシュ方向設定
+	CPlayer::DashAngle GetHoldDashAngle() { return m_HoldDashAngle; }							// ダッシュ方向取得
+	void SetInputInterval(float fInputInterval) { m_fInputInterval = fInputInterval; }			// 入力の受け付け猶予設定
+	float GetInputInterval() { return m_fInputInterval; }										// 入力の受け付け猶予取得
+	void SetTriggerInterval(float fTriggerInterval) { m_fTriggerInterval = fTriggerInterval; }	// トリガーのインターバル設定
+	float GetTriggerInterval() { return m_fTriggerInterval; }									// トリガーのインターバル取得
 	void SetBlink(bool bDash) { m_bBlink = bDash; }												// ブリンクフラグ設定
 	bool IsBlink() { return m_bBlink; }															// ブリンクフラグ取得
 
