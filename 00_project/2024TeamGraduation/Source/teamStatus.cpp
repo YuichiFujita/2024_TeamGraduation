15550df204d805dc9e09ff39934a118ab1d75464
KaiTakada <221104@st.yoshida-g.ac.jp>
2024-10-24 09:40:27 +0900

    スペシャルゲージ生成調整完了

--- 00_project/2024TeamGraduation/Source/teamStatus.cpp
+++ 00_project/2024TeamGraduation/Source/teamStatus.cpp
@@ -17,8 +17,8 @@ namespace Charm
 namespace Special
 {
 	const float VALUE_MAX = 100.0f;												// 最大値
-	const MyLib::Vector2 GAUGE_SIZE = MyLib::Vector2(50.0f, 200.0f);			// ゲージサイズ
-	const MyLib::Vector3 GAUGE_POS = MyLib::Vector3(50.0f, 100.0f, 0.0f);		// ゲージ位置
+	const MyLib::Vector2 GAUGE_SIZE = MyLib::Vector2(250.0f, 30.0f);			// ゲージサイズ
+	const MyLib::Vector3 GAUGE_POS = MyLib::Vector3(30.0f, 50.0f, 0.0f);		// ゲージ位置
 }
 
 //==========================================================================
@@ -93,20 +93,21 @@ void CTeamStatus::TeamSetting(const CGameManager::TeamSide team)
 	MyLib::Vector3 pos = MyLib::Vector3();
 	MyLib::Vector3 dest = Special::GAUGE_POS;
 
+	// チームサイド設定
 	SetTeam(team);
 
 	switch (team)
 	{
 	case CGameManager::SIDE_LEFT:
 
-		anchor = CObject2D::LEFT;
+		anchor = CObject2D::RIGHT;
 		pos += MyLib::Vector3(SCREEN_WIDTH, SCREEN_HEIGHT, 0.0f);	// 左下
 		pos += MyLib::Vector3(-dest.x, -dest.y, 0.0f);
 		break;
 
 	case CGameManager::SIDE_RIGHT:
 
-		anchor = CObject2D::RIGHT;
+		anchor = CObject2D::LEFT;
 		pos += MyLib::Vector3(0.0f, SCREEN_HEIGHT, 0.0f);	// 右下
 		pos += MyLib::Vector3(dest.x, -dest.y, 0.0f);
 		break;
@@ -138,7 +139,78 @@ void CTeamStatus::InitSpecialInfo()
 		m_sSpecialInfo.pGauge = nullptr;
 	}
 
+	//ゲージ生成
 	m_sSpecialInfo.pGauge = CObject2D::Create();
 	m_sSpecialInfo.pGauge->SetSize(Special::GAUGE_SIZE);
+
+
+
 	m_sSpecialInfo.fValueMax = Special::VALUE_MAX;
+}
+
+//==========================================================================
+// モテゲージ増加
+//==========================================================================
+void CTeamStatus::AddSpecialValue(float fValue)
+{
+	m_sSpecialInfo.fValue += fValue;
+
+	UtilFunc::Transformation::Clamp(m_sSpecialInfo.fValue, 0.0f, m_sSpecialInfo.fValueMax);
+
+	if (m_sSpecialInfo.pGauge != nullptr)
+	{
+		MyLib::Vector2 size = Special::GAUGE_SIZE;
+		float fRad = m_sSpecialInfo.fValue / m_sSpecialInfo.fValueMax;
+		
+		size.x *= fRad;
+
+		m_sSpecialInfo.pGauge->SetSize(size);
+	}
+}
+
+//==========================================================================
+// モテゲージ減少
+//==========================================================================
+void CTeamStatus::SubSpecialValue(float fValue)
+{
+	m_sSpecialInfo.fValue += fValue;
+
+	UtilFunc::Transformation::Clamp(m_sSpecialInfo.fValue, 0.0f, m_sSpecialInfo.fValueMax);
+
+	if (m_sSpecialInfo.pGauge != nullptr)
+	{
+		MyLib::Vector2 size = Special::GAUGE_SIZE;
+		float fRad = m_sSpecialInfo.fValue / m_sSpecialInfo.fValueMax;
+		
+		size.x *= fRad;
+
+		m_sSpecialInfo.pGauge->SetSize(size);
+	}
+}
+
+//==========================================================================
+// デバッグ
+//==========================================================================
+void CTeamStatus::Debug()
+{
+	//-----------------------------
+// コート
+//-----------------------------
+	if (ImGui::TreeNode("Special"))
+	{
+		ImGui::DragFloat("GaugeSize", (float*)&m_sSpecialInfo.fValue, 1.0f, 0.0f, m_sSpecialInfo.fValueMax, "%.2f");
+
+		// 位置設定
+		ImGui::TreePop();
+	
+		if (m_sSpecialInfo.pGauge != nullptr)
+		{
+			MyLib::Vector2 size = Special::GAUGE_SIZE;
+			float fRad = m_sSpecialInfo.fValue / m_sSpecialInfo.fValueMax;
+		
+			size.x *= fRad;
+
+			m_sSpecialInfo.pGauge->SetSize(size);
+		}
+	}
 }
