adc47783390027022b1d544ef21460ab067e835a
YuichFujita <221065@st.yoshida-g.ac.jp>
2024-10-24 16:20:04 +0900

    audienceAnim:objectCharaAnimを親クラスに変更

--- 00_project/2024TeamGraduation/Source/audienceAnim.h
+++ 00_project/2024TeamGraduation/Source/audienceAnim.h
@@ -13,7 +13,7 @@
 //==========================================================================
 #include "audience.h"
 #include "gamemanager.h"
-#include "object3D_Anim.h"
+#include "objectCharaAnim.h"
 
 //==========================================================================
 // クラス定義
@@ -43,19 +43,19 @@ public:
 	void Update(const float fDeltaTime, const float fDeltaRate, const float fSlowRate) override;
 	void Draw() override;
 	void Kill() override;	// 削除処理
-	inline void SetPosition(const MyLib::Vector3& pos) override	{ m_pAnim3D->SetPosition(pos); }		// 位置設定
-	inline MyLib::Vector3 GetPosition() const override			{ return m_pAnim3D->GetPosition(); }	// 位置取得
-	inline void SetMove(const MyLib::Vector3& move) override	{ m_pAnim3D->SetMove(move); }			// 移動量設定
-	inline MyLib::Vector3 GetMove() const override				{ return m_pAnim3D->GetMove(); }		// 移動量取得
-	inline void SetRotation(const MyLib::Vector3& rot) override	{ m_pAnim3D->SetRotation(rot); }		// 向き設定
-	inline MyLib::Vector3 GetRotation() const override			{ return m_pAnim3D->GetRotation(); }	// 向き取得
+	inline void SetPosition(const MyLib::Vector3& pos) override	{ m_pAnimChara->SetPosition(pos); }		// 位置設定
+	inline MyLib::Vector3 GetPosition() const override			{ return m_pAnimChara->GetPosition(); }	// 位置取得
+	inline void SetMove(const MyLib::Vector3& move) override	{ m_pAnimChara->SetMove(move); }		// 移動量設定
+	inline MyLib::Vector3 GetMove() const override				{ return m_pAnimChara->GetMove(); }		// 移動量取得
+	inline void SetRotation(const MyLib::Vector3& rot) override	{ m_pAnimChara->SetRotation(rot); }		// 向き設定
+	inline MyLib::Vector3 GetRotation() const override			{ return m_pAnimChara->GetRotation(); }	// 向き取得
 
 private:
 
 	//=============================
 	// メンバ変数
 	//=============================
-	CObject3DAnim* m_pAnim3D;	// アニメーション3D情報
+	CObjectCharaAnim* m_pAnimChara;	// キャラクターアニメーション情報
 };
 
 #endif
