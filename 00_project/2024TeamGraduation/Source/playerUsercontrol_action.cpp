dd57602a552073f7d86e87a0ae3b44f55cba933f
YuichFujita <221065@st.yoshida-g.ac.jp>
2024-11-22 16:22:57 +0900

    playerUsercontrol_action:ユーザー変更操作の作成

--- 00_project/2024TeamGraduation/Source/playerUsercontrol_action.cpp
+++ 00_project/2024TeamGraduation/Source/playerUsercontrol_action.cpp
@@ -177,104 +177,6 @@ void CPlayerUserControlAction::Charm(CPlayer* player, const float fDeltaTime, co
 //==========================================================================
 void CPlayerUserControlAction::UserChange(CPlayer* player, const float fDeltaTime, const float fDeltaRate, const float fSlowRate)
 {
-#if 0
-	// インプット情報取得
-	CInputGamepad* pPad = CInputGamepad::GetInstance();
-
-	// プレイヤー操作権インデックス取得
-	const int nPadIdx = player->GetMyPlayerIdx();
-
-	// 右スティックのトリガー入力検知フラグ
-	const bool bInput = pPad->GetRStickTrigger(nPadIdx, CInputGamepad::STICK_AXIS::STICK_X)
-					 || pPad->GetRStickTrigger(nPadIdx, CInputGamepad::STICK_AXIS::STICK_Y);
-
-	if (bInput && !CPlayerManager::GetInstance()->IsUserChange())
-	{
-
-		MyLib::Vector3 posThis = player->GetPosition();	// 自身のプレイヤー位置
-		CPlayer* pChange = nullptr;						// 変更プレイヤー
-		float fRotMinDiff = D3DX_PI * 2.0f;				// 
-		float fRotStick = pPad->GetStickRotR(nPadIdx);	// スティック向き
-
-		// TODO：方向エフェクト
-#if 0
-		MyLib::Vector3 posStick = player->GetPosition();	// 自身のプレイヤー位置
-		posStick.x += sinf(fRotStick) * 150.0f;
-		posStick.z += cosf(fRotStick) * 150.0f;
-
-		// 演出
-		CEffect3D::Create(
-			posStick + MyLib::Vector3(0.0f, 50.0f, 0.0f),
-			MyLib::Vector3(0.0f, 0.0f, 0.0f),
-			MyLib::color::White(),
-			20.0f, 6.0f, CEffect3D::MOVEEFFECT_NONE, CEffect3D::TYPE_NORMAL);
-#endif
-
-		CListManager<CPlayer> list = CPlayerManager::GetInstance()->GetInList(player->GetTeam());	// 内野プレイヤーリスト
-		std::list<CPlayer*>::iterator itr = list.GetEnd();	// 最後尾イテレーター
-		while (list.ListLoop(itr))
-		{ // リスト内の要素数分繰り返す
-
-			CPlayer* pItrPlayer = (*itr);	// プレイヤー情報
-
-			// 既にユーザーの場合次へ
-			if (pItrPlayer->GetBaseType() == CPlayer::EBaseType::TYPE_USER) { continue; }
-
-			float fRotSpace = pItrPlayer->GetPosition().AngleXZ(posThis);
-			float fRotStickA = (fRotStick + D3DX_PI);
-			float fRotSpaceA = (fRotSpace + D3DX_PI);
-			float fRotDiff = fabsf(fRotStickA - fRotSpaceA);
-
-			// TODO：方向エフェクト
-#if 0
-			MyLib::Vector3 posDir = player->GetPosition();	// 自身のプレイヤー位置
-			posDir.x += sinf(fRotSpace) * 125.0f;
-			posDir.z += cosf(fRotSpace) * 125.0f;
-
-			// 演出
-			CEffect3D::Create(
-				posDir + MyLib::Vector3(0.0f, 50.0f, 0.0f),
-				MyLib::Vector3(0.0f, 0.0f, 0.0f),
-				MyLib::color::Red(),
-				15.0f, 6.0f, CEffect3D::MOVEEFFECT_NONE, CEffect3D::TYPE_NORMAL);
-#endif
-
-			if (pChange == nullptr || fRotDiff < fRotMinDiff)
-			{ // より差分が小さい向きの場合
-
-				// 今回の差分向きを保存
-				fRotMinDiff = fRotDiff;
-
-				// 変更するプレイヤーを保存
-				pChange = pItrPlayer;
-			}
-		}
-
-		if (pChange != nullptr)
-		{ // 変更対象が存在する場合
-
-			// TODO：方向エフェクト
-#if 0
-			float fRotChange = pChange->GetPosition().AngleXZ(posThis);
-			MyLib::Vector3 posChange = player->GetPosition();	// 自身のプレイヤー位置
-			posChange.x += sinf(fRotChange) * 175.0f;
-			posChange.z += cosf(fRotChange) * 175.0f;
-
-			// 演出
-			CEffect3D::Create(
-				posChange + MyLib::Vector3(0.0f, 50.0f, 0.0f),
-				MyLib::Vector3(0.0f, 0.0f, 0.0f),
-				MyLib::color::Blue(),
-				10.0f, 6.0f, CEffect3D::MOVEEFFECT_NONE, CEffect3D::TYPE_NORMAL);
-#endif
-
-			// ベースの入れ替え
-			CPlayerManager::GetInstance()->SwapBase(player, pChange);
-
-			CPlayerManager::GetInstance()->SetEnableUserChange(true);
-		}
-	}
-#else
 	// 最良目標情報
 	struct SBestDest
 	{
@@ -290,27 +192,26 @@ void CPlayerUserControlAction::UserChange(CPlayer* player, const float fDeltaTim
 	const bool bInput = pPad->GetRStickTrigger(nPadIdx, CInputGamepad::STICK_AXIS::STICK_X)
 					 || pPad->GetRStickTrigger(nPadIdx, CInputGamepad::STICK_AXIS::STICK_Y);
 
-	//if (bInput && !pPlayerManager->IsUserChange())
+	if (bInput && !pPlayerManager->IsUserChange())
 	{ // 右スティックが入力されている且つ、まだ誰も同フレームでこの操作をしていない場合
 
 		CListManager<CPlayer> list = pPlayerManager->GetInList(player->GetTeam());	// 内野プレイヤーリスト
 		std::list<CPlayer*>::iterator itr = list.GetEnd();	// 最後尾イテレーター
 		MyLib::Vector3 posThis = player->GetPosition();		// 操作プレイヤー位置
 		float fRotStick = pPad->GetStickRotR(nPadIdx);		// スティック向き
-		float fRotStickA = fRotStick;
-
-		// TODO：スティック向きのデバッグ表示
-		GET_MANAGER->GetDebugProc()->Print("スティック向き：%f\n", fRotStickA);
 
 		// 向き基準の最良目標情報
 		SBestDest bestRot;
 		bestRot.pPlayer = nullptr;
 		bestRot.fMinValue = D3DX_PI * 2.0f;
 
+		// TODO：距離基準も必要かな？
+#if 0
 		// 距離基準の最良目標情報
 		SBestDest bestDis;
 		bestDis.pPlayer = nullptr;
 		bestDis.fMinValue = CGameManager::GetInstance()->GetHalfCourtDiagonal();
+#endif
 
 		while (list.ListLoop(itr))
 		{ // リスト内の要素数分繰り返す
@@ -320,19 +221,13 @@ void CPlayerUserControlAction::UserChange(CPlayer* player, const float fDeltaTim
 			// 既にユーザーの場合次へ
 			if (pItrPlayer->GetBaseType() == CPlayer::EBaseType::TYPE_USER) { continue; }
 
-#if 1
+			// 操作プレイヤーから見たAIの向きを計算
+			float fRotSpace = posThis.AngleXZ(pItrPlayer->GetPosition());
 
-#if 1
-			float fRotSpace = pItrPlayer->GetPosition().AngleXZ(posThis);	// 操作プレイヤーから見たAIの向き
-#else
-			float fRotSpace = posThis.AngleXZ(pItrPlayer->GetPosition());	// 操作プレイヤーから見たAIの向き
-#endif
-			float fRotSpaceA = fRotSpace + D3DX_PI;
-			float fRotDiff = fRotStickA - fRotSpaceA;
-
-			// TODO：差分向きのデバッグ表示
-			GET_MANAGER->GetDebugProc()->Print("ＡＩ向き：%f\n", fRotSpaceA);
-			GET_MANAGER->GetDebugProc()->Print("差分向き：%f\n", fRotDiff);
+			// 向きの差分を計算
+			float fRotDiff = fRotStick - fRotSpace;				// 差分の計算
+			UtilFunc::Transformation::RotNormalize(fRotDiff);	// 正規化 (3.14～-3.14)
+			fRotDiff = fabsf(fRotDiff);							// 絶対値化 (0.0f～3.14)
 
 			// 向き基準の最良目標を更新
 			if (bestRot.pPlayer == nullptr || bestRot.fMinValue < fRotDiff)
@@ -344,8 +239,8 @@ void CPlayerUserControlAction::UserChange(CPlayer* player, const float fDeltaTim
 				// 目標プレイヤーを保存
 				bestRot.pPlayer = pItrPlayer;
 			}
-#endif
 
+			// TODO：距離基準も必要かな？
 #if 0
 			// 距離基準の最良目標を更新
 			if (bestDis.pPlayer == nullptr || bestDis.fMinValue < )
@@ -361,17 +256,14 @@ void CPlayerUserControlAction::UserChange(CPlayer* player, const float fDeltaTim
 		}
 
 		// 最良の目標プレイヤーに操作権を渡す
-#if 0
-		if (pChange != nullptr)
+		if (bestRot.pPlayer != nullptr)
 		{ // 変更対象が存在する場合
 
 			// ベースの入れ替え
-			pPlayerManager->SwapBase(player, pChange);
+			pPlayerManager->SwapBase(player, bestRot.pPlayer);
 
 			// ユーザー変更操作済みにする
 			pPlayerManager->SetEnableUserChange(true);
 		}
-#endif
 	}
-#endif
 }
