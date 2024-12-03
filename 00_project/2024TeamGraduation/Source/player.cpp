a8dfd9ec7fcdf759ae08e5fd5a57a1e62886d317
YuichFujita <221065@st.yoshida-g.ac.jp>
2024-12-03 10:19:56 +0900

    ball:パス状態を途中で撮ったら外野のUserをAIに戻すように

--- 00_project/2024TeamGraduation/Source/player.cpp
+++ 00_project/2024TeamGraduation/Source/player.cpp
@@ -2382,10 +2382,14 @@ void CPlayer::Debug()
 		ImGui::Text("Motion : [%s]", magic_enum::enum_name(motionType));
 		ImGui::Text("Action : [%s]", magic_enum::enum_name(action));
 		ImGui::Text("State : [%s]", magic_enum::enum_name(m_state));
-		ImGui::Text("StateTime: [%.2f]", m_fStateTime);
-		ImGui::Text("bDash: [%d]", m_bDash);
-		ImGui::Text("bBrake: [%d]", m_bBrake);
-		ImGui::Text("InputAngleCtr: [%.2f]", m_pBase->GetPlayerControlMove()->GetInputAngleCtr());
+		ImGui::Text("StateTime : [%.2f]", m_fStateTime);
+		ImGui::Text("bDash : [%d]", m_bDash);
+		ImGui::Text("bBrake : [%d]", m_bBrake);
+		ImGui::Text("InputAngleCtr : [%.2f]", m_pBase->GetPlayerControlMove()->GetInputAngleCtr());
+
+		ImGui::Text("typeBase : [%s]", magic_enum::enum_name(GetBaseType()));
+		ImGui::Text("typeTeam : [%s]", magic_enum::enum_name(m_typeTeam));
+		ImGui::Text("typeArea : [%s]", magic_enum::enum_name(m_typeArea));
 
 		if (angle != nullptr)
 		{
@@ -2393,7 +2397,7 @@ void CPlayer::Debug()
 		}
 		else
 		{
-			ImGui::Text("InputAngle: [error]");
+			ImGui::Text("InputAngle : [error]");
 		}
 
 #if 0
