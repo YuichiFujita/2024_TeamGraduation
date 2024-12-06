c5fba7e154110b7aa51cd8a92c711bc7968db9b2
KaiTakada <221104@st.yoshida-g.ac.jp>
2024-12-06 10:08:01 +0900

    ジャスト投げなんとボール投げれます！

--- 00_project/2024TeamGraduation/Source/player.cpp
+++ 00_project/2024TeamGraduation/Source/player.cpp
@@ -974,6 +974,7 @@ void CPlayer::AttackAction(CMotion::AttackInfo ATKInfo, int nCntATK)
 	case EMotion::MOTION_THROW:
 	case EMotion::MOTION_THROW_RUN:
 	case EMotion::MOTION_THROW_DROP:
+	case EMotion::MOTION_THROW_JUST:
 
 		if (m_pBall != nullptr)
 		{// 通常投げ
@@ -982,6 +983,7 @@ void CPlayer::AttackAction(CMotion::AttackInfo ATKInfo, int nCntATK)
 		break;
 
 	case EMotion::MOTION_THROW_JUMP:
+	case EMotion::MOTION_THROW_JUST_JUMP:
 
 		if (m_pBall != nullptr)
 		{// ジャンプ投げ
