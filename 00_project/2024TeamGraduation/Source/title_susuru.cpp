93fba2d91eb66c8577890fbca2bba2b898f691c8
Seiga Soum <221120@st.yoshida-g.ac.jp>
2024-12-16 16:25:17 +0900

    SUSURU

---
+++ 00_project/2024TeamGraduation/Source/title_susuru.cpp
@@ -0,0 +1,175 @@
+//=============================================================================
+// 
+// SUSURUシーン処理 [title_susuru.cpp]
+// Author : 相馬靜雅
+// 
+//=============================================================================
+#include "title_susuru.h"
+#include "manager.h"
+#include "game.h"
+#include "input.h"
+#include "entry_setupTeam.h"
+#include "dressup.h"
+#include "playerManager.h"
+
+//==========================================================================
+// 定数定義
+//==========================================================================
+namespace
+{
+	const std::string TEXTURE = "data\\TEXTURE\\title\\susuru.png";		// テクスチャ
+	const MyLib::Vector2 TEXTSIZE = MyLib::Vector2((SCREEN_WIDTH / CTitle_SUSURU::NUM_TEXT) * 0.75f, SCREEN_HEIGHT * 0.5f);	// サイズ
+}
+
+namespace StateTime
+{
+	const float WAIT = 0.2;		// 待機
+	const float SCROLL = 1.0f;	// 送り
+}
+
+//==========================================================================
+// 関数ポインタ
+//==========================================================================
+CTitle_SUSURU::STATE_FUNC CTitle_SUSURU::m_StateFunc[] =	// 状態関数
+{
+	&CTitle_SUSURU::StateWait,		// 待機
+	&CTitle_SUSURU::StateScroll,	// 文字送り
+};
+
+//==========================================================================
+// コンストラクタ
+//==========================================================================
+CTitle_SUSURU::CTitle_SUSURU() : CTitleScene()
+{
+	// 状態
+	m_state = EState::STATE_WAIT;		// 状態
+	m_fStateTime = 0.0f;				// 状態時間
+}
+
+//==========================================================================
+// デストラクタ
+//==========================================================================
+CTitle_SUSURU::~CTitle_SUSURU()
+{
+
+}
+
+//==========================================================================
+// 初期化処理
+//==========================================================================
+HRESULT CTitle_SUSURU::Init()
+{
+	
+	// 文字
+	float u = (1.0f / NUM_TEXT);
+	for (int i = 0; i < NUM_TEXT; i++)
+	{
+		// 生成
+		m_apText[i] = CObject2D::Create(4);
+
+		// 種類設定
+		m_apText[i]->SetType(CObject::TYPE::TYPE_OBJECT2D);
+		m_apText[i]->SetAnchorType(CObject2D::AnchorPoint::TOP_CENTER);
+
+		// テクスチャ設定
+		CTexture* pTexture = CTexture::GetInstance();
+		int texID = CTexture::GetInstance()->Regist(TEXTURE);
+		m_apText[i]->BindTexture(texID);
+
+		// サイズ設定
+		m_apText[i]->SetSize(MyLib::Vector2(TEXTSIZE.x, 0.0f));
+		m_apText[i]->SetSizeOrigin(TEXTSIZE);
+
+		// 位置設定
+		m_apText[i]->SetPosition(MyLib::Vector3(TEXTSIZE.x * 0.75f, 0.0f, 0.0f) + MyLib::Vector3(SCREEN_WIDTH - TEXTSIZE.x * 1.5f * i, 0.0f, 0.0f));
+
+		// UV
+		D3DXVECTOR2* pTex = m_apText[i]->GetTex();
+		pTex[0].x = pTex[2].x = 1.0f - u * i;
+		pTex[1].x = pTex[3].x = 1.0f - (u * i) + u;
+	}
+
+	// 送る文字インデックス
+	m_nIdxScroll = 0;
+
+	return S_OK;
+}
+
+//==========================================================================
+// 終了処理
+//==========================================================================
+void CTitle_SUSURU::Uninit()
+{
+	delete this;
+}
+
+//==========================================================================
+// 更新処理
+//==========================================================================
+void CTitle_SUSURU::Update(const float fDeltaTime, const float fDeltaRate, const float fSlowRate)
+{
+	// 親の更新
+	CTitleScene::Update(fDeltaTime, fDeltaRate, fSlowRate);
+
+	// 状態更新
+	UpdateState(fDeltaTime, fDeltaRate, fSlowRate);
+}
+
+//==========================================================================
+// 状態更新
+//==========================================================================
+void CTitle_SUSURU::UpdateState(const float fDeltaTime, const float fDeltaRate, const float fSlowRate)
+{
+	// 状態タイマー加算
+	m_fStateTime += fDeltaTime * fSlowRate;
+
+	// 状態更新
+	(this->*(m_StateFunc[m_state]))(fDeltaTime, fDeltaRate, fSlowRate);
+}
+
+//==========================================================================
+// 待機
+//==========================================================================
+void CTitle_SUSURU::StateWait(const float fDeltaTime, const float fDeltaRate, const float fSlowRate)
+{
+	if (m_fStateTime >= StateTime::WAIT)
+	{
+		m_fStateTime = 0.0f;
+		m_state = EState::STATE_SCROLL;
+
+		// 送る文字更新
+		m_nIdxScroll++;
+
+		if (m_nIdxScroll >= CTitle_SUSURU::NUM_TEXT)
+		{// 全て更新
+			CTitle::GetInstance()->ChangeScene(CTitle::ESceneType::SCENETYPE_CONTROLLWAIT);
+		}
+	}
+}
+
+//==========================================================================
+// 文字送り
+//==========================================================================
+void CTitle_SUSURU::StateScroll(const float fDeltaTime, const float fDeltaRate, const float fSlowRate)
+{
+	// 文字送り
+	MyLib::Vector2 size = m_apText[m_nIdxScroll]->GetSize();
+
+	// サイズ設定
+	size.y = UtilFunc::Correction::EasingLinear(0.0f, TEXTSIZE.y, 0.0f, StateTime::SCROLL, m_fStateTime);
+	m_apText[m_nIdxScroll]->SetSize(size);
+
+	
+
+		// 生成
+		D3DXVECTOR2* pTex = m_apText[m_nIdxScroll]->GetTex();
+
+		// UVをサイズに合わせる
+		pTex[2].y = pTex[3].y = m_apText[m_nIdxScroll]->GetSize().y / m_apText[m_nIdxScroll]->GetSizeOrigin().y;
+
+	if (m_fStateTime >= StateTime::SCROLL)
+	{
+		m_fStateTime = 0.0f;
+		m_state = EState::STATE_WAIT;
+	}
+}
---
+++ 00_project/2024TeamGraduation/Source/title_susuru.h
@@ -0,0 +1,91 @@
+//=============================================================================
+//
+// SUSURUシーン処理 [title_susuru.h]
+// Author : 相馬靜雅
+//
+//=============================================================================
+#ifndef _ENTRY_DRESSUP_H_		// このマクロ定義がされていなかったら
+#define _ENTRY_DRESSUP_H_		// 二重インクルード防止のマクロを定義する
+
+//==========================================================================
+// インクルードファイル
+//==========================================================================
+#include "titlescene.h"
+#include "player.h"
+
+//==========================================================================
+// 前方宣言
+//==========================================================================
+class CDressup;
+
+//==========================================================================
+// クラス定義
+//==========================================================================
+// SUSURUシーンクラス
+class CTitle_SUSURU : public CTitleScene
+{
+public:
+
+	//=============================
+	// 定数
+	//=============================
+	static constexpr int NUM_TEXT = 10;		// テキストの数
+
+	//=============================
+	// 列挙型定義
+	//=============================
+	enum EState
+	{
+		STATE_WAIT = 0,	// 待機
+		STATE_SCROLL,	// 文字送り
+		STATE_MAX
+	};
+
+	CTitle_SUSURU();
+	~CTitle_SUSURU();
+
+	//=============================
+	// メンバ関数
+	//=============================
+	virtual HRESULT Init() override;		// 初期化
+	virtual void Uninit() override;		// 終了
+	virtual void Update(const float fDeltaTime, const float fDeltaRate, const float fSlowRate) override;	// 更新
+
+	//--------------------------
+	// その他
+	//--------------------------
+
+private:
+
+	//=============================
+	// 関数リスト
+	//=============================
+	typedef void(CTitle_SUSURU::* STATE_FUNC)(const float, const float, const float);
+	static STATE_FUNC m_StateFunc[];	// 状態関数
+
+	//=============================
+	// メンバ関数
+	//=============================
+	// 状態関数
+	//-----------------------------
+	virtual void UpdateState(const float fDeltaTime, const float fDeltaRate, const float fSlowRate);			// 状態更新
+	void StateWait(const float fDeltaTime, const float fDeltaRate, const float fSlowRate);		// 待機
+	void StateScroll(const float fDeltaTime, const float fDeltaRate, const float fSlowRate);	// 文字送り
+
+	//=============================
+	// メンバ変数
+	//=============================
+	//-----------------------------
+	// 状態
+	//-----------------------------
+	EState m_state;			// 状態
+	float m_fStateTime;		// 状態時間
+
+	//-----------------------------
+	// その他
+	//-----------------------------
+	int m_nIdxScroll;	// 送る文字インデックス
+	CObject2D* m_apText[NUM_TEXT];	// 文字
+};
+
+#endif
