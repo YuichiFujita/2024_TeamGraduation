7ee247daaded8985209313bb621a6c5bf1a23151
Seiga Soum <221120@st.yoshida-g.ac.jp>
2025-01-27 09:13:35 +0900

    やばいテクスチャが消えた

--- 00_project/2024TeamGraduation/Source/tutorial.h
+++ 00_project/2024TeamGraduation/Source/tutorial.h
@@ -1,6 +1,6 @@
 //=============================================================================
 // 
-//  タイトルヘッダー [tutorial.h]
+//  チュートリアルヘッダー [tutorial.h]
 //  Author : 相馬靜雅
 // 
 //=============================================================================
@@ -8,35 +8,63 @@
 #ifndef _TUTORIAL_H_
 #define _TUTORIAL_H_	// 二重インクルード防止
 
+//==========================================================================
+// インクルードファイル
+//==========================================================================
 #include "manager.h"
-
-class CTutorialLogo;
 
 //==========================================================================
 // クラス定義
 //==========================================================================
-// タイトルクラス
+// チュートリアルクラス
 class CTutorial : public CScene
 {
 public:
 
+	//=============================
+	// コンストラクタ,デストラクタ
+	//=============================
 	CTutorial();
 	~CTutorial();
 
-	// オーバーライドされた関数
+	//=============================
+	// オーバーライド関数
+	//=============================
 	HRESULT Init() override;
 	void Uninit() override;
 	void Update(const float fDeltaTime, const float fDeltaRate, const float fSlowRate) override;
 	void Draw() override;
 
+	//=============================
+	// メンバ関数
+	//=============================
+	
+
+	//=============================
 	// 静的関数
-	static CTutorial* GetInstance();	// インスタンス取得
-	static CTutorial* Create();		// 生成処理
+	//=============================
+	static CTutorial* GetInstance() { return m_pThisPtr; }	// インスタンス取得
+	static CTutorial* Create();							// 生成処理
 
 private:
 
-	CTutorialLogo* m_pLogo;		// ロゴのポインタ
-	static CTutorial* m_pThisPtr;	// 自身のポインタ
+	//=============================
+	// 関数ポインタ
+	//=============================
+	typedef void(CTutorial::* SCENE_FUNC)(const float, const float, const float);
+	static SCENE_FUNC m_SceneFunc[];
+
+	//=============================
+	// メンバ関数
+	//=============================
+	void SceneNone(const float fDeltaTime, const float fDeltaRate, const float fSlowRate);			// なにもなし
+	void SceneFadeInLogo(const float fDeltaTime, const float fDeltaRate, const float fSlowRate);	// ロゴフェードイン
+	void SceneFadeOutLoGo(const float fDeltaTime, const float fDeltaRate, const float fSlowRate);	// ロゴフェードアウト
+
+	//=============================
+	// メンバ変数
+	//=============================
+	static CTutorial* m_pThisPtr;		// 自身のポインタ
 };
 
 
